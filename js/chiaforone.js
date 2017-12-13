5b92d5120931fdb2fddf597c5fb5b11982275f38
Daway Cai <caixnet@outlook.com>
2017-12-13 05:14:12 -0800

    chia forone keybard ime demo

--- js/chiaforone.js
+++ js/chiaforone.js
@@ -2,6 +2,7 @@
  * 4 points 1 string ( Chia IME keyboard )
  * @author Daway.Cai (Cai,GuangXian)
  * @email caixnet@outlook.com
+ * chia forone is reserved.
  * Application number: 2014800745031
  * International Application : PCT/CN2014/092045, 
  * International Publication : WO2016/082081
@@ -48,21 +49,55 @@ var chia = {
         return $(this.chiaImeBoxID).css("background-color", aColor);
     },
     clearKeys: function () { return this.keys = ""; },
+    setCusor: function (iCursor) { this.cursor = iCursor; },
+    getCusorPos: function () {
+        $(this.textID).focus();
+        return $(this.textID).get(0).selectionStart;
+    },
     deleteKey: function () {
+        //
+        //var start = $("#chiatext").get(0).selectionStart;
+        //var end = $("#chiatext").get(0).selectionEnd;
+        //console.log(start, end);
+        //
         this.docs = $(this.textID).val();
-        $(this.textID).val(this.docs.substring(0, this.docs.length - 1));
+        this.setCusor(this.getCusorPos());
+        if(this.cursor == this.docs.length){
+            this.docs = this.docs.substring(0, this.cursor-1) + this.docs.substring(this.cursor, this.docs.length);
+            this.setCusor(this.cursor-1);
+        }else{
+            this.docs = this.docs.substring(0, this.cursor-2) + this.docs.substring(this.cursor-1, this.docs.length);
+            this.setCusor(this.cursor-2);
+        }
+        $(this.textID).val(this.docs);
+        $(this.textID).focus();
+        $(this.textID).get(0).setSelectionRange(this.cursor, this.cursor);
         this.setEndIdColor();
         this.clearKeys();
     },
     enterKey: function () {
         this.docs = $(this.textID).val();
-        $(this.textID).val(this.docs + "\n");
+        this.setCusor(this.getCusorPos());
+        if(this.cursor == this.docs.length){
+            this.docs = this.docs.substring(0, this.cursor) + "\n" ;
+            this.setCusor(this.cursor+1);
+        }else{
+            this.docs = this.docs.substring(0, this.cursor-1) + "\n" + this.docs.substring(this.cursor-1, this.docs.length);
+            this.setCusor(this.cursor);
+        }
+        $(this.textID).val(this.docs);
+        $(this.textID).get(0).setSelectionRange(this.cursor, this.cursor);
+        $(this.textID).focus();
         this.clearKeys();
     },
     writeToText: function () {
         this.docs = $(this.textID).val();
-        this.docs += this.getChar(this.keys);
+        this.setCusor(this.getCusorPos());
+        this.docs = this.docs.substring(0, this.cursor) + this.getChar(this.keys) + this.docs.substring(this.cursor, this.docs.length);
+        this.setCusor(this.cursor + this.getChar(this.keys).length);
         $(this.textID).val(this.docs);
+        $(this.textID).focus();
+        $(this.textID).get(0).setSelectionRange(this.cursor, this.cursor);
         this.setEndIdColor();
         this.clearKeys();
     },
@@ -208,7 +243,7 @@ function onMouseOver() {
             if (chia.mouseOn) {
                 chia.keys += chia.getID(this);
             } else {
-            //
+                //
             }
         });
     }
@@ -272,6 +307,12 @@ function onMouseClick() {
     }
     ///
 }
+function hideKeyboard() {
+    document.activeElement.blur();
+    $("input").blur();
+    $("textarea").blur();
+    $("#chiatext").blur();
+};
 // binding mouseEvent
 function onMouseEvent() {
     onMouseDblClick();
@@ -288,6 +329,7 @@ function hideLoadLabel() {
 /*END Swipe*/
 // main ready document to check chia keyboard.
 $(document).ready(function () {
+    hideKeyboard();
     onMouseEvent();
     hideLoadLabel();
 });
@@ -297,97 +339,103 @@ $(document).ready(function () {
 chia.codes = {
     /* chia Code reserve code */
     "1341": "a", "1431": "a",
-    "124134": "b", "143124": "b", "134124": "b", "134214": "b", "142134": "b",
-    "1234": "c", "4321": "c",
+    "124134": "b", "143124": "b", "134124": "b", "134214": "b", "142134": "b", "124314":"b", "424": "b",
+    "1234": "c",
     "1241": "d", "1421": "d",
     "12342": "e", "12432": "e",
     "14324": "f", "14234": "f",
-    "123": "g", /*"321": "}",*/
+    "123": "g",
     "14231": "h", "13241": "h",
     "13": "i",
-    "134": "j", "431": ">",
+    "134": "j",
     "124": "k",
-    "143": "l", "341": "|",
-    "142132": "m", "132142": "m", "124132": "m",
+    "143": "l", "341": "(", "432": ")",
+    "142132": "m", "132142": "m", "124132": "m","123142": "m","142312":"m","132412":"m", "313": "m",
     "1423": "n",
     "12341": "o", "14321": "o",
     "12314": "p", "13214": "p",
     "14213": "q", "12413": "q",
-    "123413": "r", "132143": "r", "134123": "r", "143123": "r", "143213": "r",
+    "123413": "r", "132143": "r", "134123": "r", "143123": "r", "143213": "r", "123143":"r","131": "r",
     "12431": "s", "13421": "s",
     "1231": "t", "1321": "t",
     "1432": "u",
-    "142": "v", /*"241": "[",*/
+    "142": "v",
     "1342": "w",
     "1324": "x",
-    "132": "y", /*"231": "]",*/
+    "132": "y",
     "1243": "z",
     "24": "1",
     "2134": "2",
-    "2143": "3", "3412": "3",
-    "243": "4", /*"342": "<",*/
+    "2143": "3", "3412": "[",
+    "243": "4",
     "2432": "5", "2342": "5",
     "21431": "6", "21341": "6",
+    "23413": "6", "23143": "6",
     "214": "7",
     "2413": "8",
-    "23413": "9", "23143": "9",
-    "214324": "0", "241234": "0", "243214": "0", "234124": "0", "214234": "0",
+    "21423": "9", "24123": "9",
+    "214324": "0", "241234": "0", "243214": "0", "234124": "0", "214234": "0", "21432": "0", "23412": "0",
     "d1341": "A", "d1431": "A",
-    "d124134": "B", "d143124": "B", "d134124": "B", "d134214": "B", "d142134": "B",
-    "d1234": "C", "d4321": "C",
+    "d124134": "B", "d143124": "B", "d134124": "B", "d134214": "B", "d142134": "B", "d124314":"B", 
+    "d1234": "C", "d4321": "",
     "d1241": "D", "d1421": "D",
     "d12342": "E", "d12432": "E",
     "d14324": "F", "d14234": "F",
     "d123": "G", "d321": "}",
     "d14231": "H", "d13241": "H",
     "d13": "I",
-    "d134": "J", /*"d431": ">",*/
+    "d134": "J",
     "d124": "K",
-    "d143": "L", /*"d341": "|",*/
-    "d142132": "M", "d132142": "M", "d124132": "M",
+    "d143": "L",
+    "d142132": "M", "d132142": "M", "d124132": "M","d123142": "M","d142312":"M","d132412":"M",
     "d1423": "N",
     "d12341": "O", "d14321": "O",
     "d12314": "P", "d13214": "P",
     "d14213": "Q", "d12413": "Q",
-    "d123413": "R", "d132143": "R", "d134123": "R", "d143123": "R", "d143213": "R",
+    "d123413": "R", "d132143": "R", "d134123": "R", "d143123": "R", "d143213": "R", "d123143":"R", "d131": "R",
     "d12431": "S", "d13421": "S",
     "d1231": "T", "d1321": "T",
     "d1432": "U",
-    "d142": "V", /*"d241": "[",*/
+    "d142": "V",
     "d1342": "W",
     "d1324": "X",
-    "d132": "Y", /*"d231": "]",*/
+    "d132": "Y",
     "d1243": "Z",
-    "411341": "A", "411431": "A",
-    "41124134": "B", "41143124": "B", "41134124": "B",
-    "411234": "C",
-    "411241": "", "411421": "",
-    "4112342": "E", "4112432": "E",
-    "4114324": "F", "4114234": "F",
-    "41123": "G",
-    "4114231": "H", "4113241": "H",
-    "4113": "I",
-    "41134": "J",
-    "41143": "L",
-    "41142132": "M", "41132142": "M", "41124132": "M",
-    "411423": "N",
-    "4112341": "O", "4114321": "O",
-    "4112314": "P", "4113214": "P",
-    "4114213": "Q", "4112413": "Q",
-    "41123413": "R", "41132143": "R", "41134123": "R", "41143123": "R", "41143213": "R",
-    "4112431": "S", "4113421": "S",
-    "411231": "T", "411321": "T",
-    "411432": "U",
-    "41142": "V",
-    "411342": "W",
-    "411324": "X",
-    "41132": "Y",
-    "411243": "Z",
-    /* Customerize define
+    "34": "_",
+    "43": "=", "4213": "=",
+    "32": "!",
+    "23": "|",
+    "3413": "@", "3143": "@",
+    "34123": "#", "32143": "#",
+    "4312": "$", "3421": "$", "3423": "%", "3243": "%",
+    "314": "^", "413": "^",
+    "34213": "&", "31243": "&", "3124": "&",
+    "3142": "*", "4132": "~", "2314": "`", "312": "`", "324": "`",
+    "242": "+","2121": "+","4231": "+",
+    "14": "'", "41": '"', 
+    "414": ",", "323": ".",
+    "141": ";", "232": ":",
+    "434": "<", "431": ">",
+    "343": "<", "342": "<",
+    "212": "{", "34124": "{", "34214": "{", "42143": "{", "41243": "{",
+    "121": "}", "31234": "}", "32134": "}", "43213": "}", "43123": "}",
+    "321": "?",
+    "4321": "]",
+    "213": "7",
+    "42": "/", "31": "\\",
+    "21": "-",
+    "12": " ",
+    "423": "    ",
+    "41324":"Hello, World!"
+    // define customerize keys
+    /*"d231": "]","d241": "[",
+    "d341": "|","d431": ">",
+    "231": "]","241": "[",
+     Customerize define
         "41": "SHIFT",
         "3214": "CTRL", "4123": "CTRL",
         "314": "ALT", "413": "ALT",
-        "234": "ENTER", "432": "ENTER",
+        "234": "ENTER",
         "14": "FN",
         "1441": "CAPSLOCK",
         "1412": "TAB",
@@ -403,30 +451,18 @@ chia.codes = {
         "14214324": "F10", "14241234": "F10", "14243214": "F10", "14234124": "F10", "14214234": "F10",
         "14142": "F11",
         "1421324": "F12", "1423124": "F12",
-        "14213243": "F13", "14243213": "F13", "14234213": "F13",
-        "423": "ELETE", "324": "ELETE",
-        "34": "LEFT",
-        "43": "RIGHT",
-        "32": "UP",
-        "23": "OWN",
-        "21": "BACKSPACE",
-        "12": " ",
+        "14213243": "F13", "14243213": "F13", "14234213": "F13",  
         "213243": "ESC", "243213": "ESC", "234213": "ESC",
-        "4132": "`",
         "414132": "~",
-        "4213": "=",
         "414213": "+",
-        "4142": "_",
-        "34124": "[", "34214": "[", "42143": "[", "41243": "[",
-        "32134": "]", "31234": "]", "43213": "]", "43123": "]",
+        "4142": "_", 
+        "32134": "]", "31234": "]", "43213": "}", "43123": "}",
         "4134124": "{", "4134214": "{", "4142143": "{", "4141243": "{",
         "4132134": "}", "4131234": "}", "4143213": "}", "4143123": "}",
-        "312": "\\", "213": "\\", "31": "\\",
-        "42": "/",
         "41312": "|", "41213": "|",
         "13423": ",", "13243": "", "34231": ",", "32431": ",",
         "24134": ".", "24134": ".", "41342": ".", "43142": ".",
-        "32142": "?", "32412": "?", "21423": "?", "24123": ".",
+        "32142": "?", "32412": "?",
         "312342": ";", "342132": ";", "321342": ";", "324312": ";", "312432": ";", "234213": ";", "213243": ";", "243123": ";",
         "413423": "'", "423413": "'", "431423": "'", "432413": "'", "431423": "'", "234213": "'", "213243": "'", "243123": "'",
         "4124": "!",
@@ -439,7 +475,11 @@ chia.codes = {
         "412413": "*",
         "4123413": "(", "4123143": "(",
         "41214324": ")", "41241234": ")", "41243214": ")", "41234124": ")", "41214234": ")"
-    */
+        */
+
+
+
+    //
 };
 
 
